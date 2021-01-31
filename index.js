6b1877f11b74a8ac72580d7a4fa9024bb01d7852
FurkanPortakal <furkanportakalx@gmail.com>
2021-02-01 02:00:48 +0300

    :tada: initial commit
    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// listen
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`running on ${PORT}`));
