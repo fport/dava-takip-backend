d316f85c1826d89c621fa88f8fae691c7035c21b
FurkanPortakal <furkanportakalx@gmail.com>
2021-02-02 22:20:08 +0300

    :art: changed root
    changed root

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 });
 
 // listen
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`running on ${PORT}`));
